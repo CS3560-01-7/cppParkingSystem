3ab3258a9a4cd3df0a0b751ba42ef718252a36e4
LynnTaka <36210389+LynnTaka@users.noreply.github.com>
2022-03-14 14:35:00 -0700

    Update Skeleton.java

--- Skeleton.java
+++ Skeleton.java
@@ -1,11 +1,19 @@
 /*
 Project name: Skeleton.java
 
-Contributers: Lynn Takahashi, 
+Contributers: Lynn Takahashi, Ethan Vazquez, Ly Rivera, Amal
 
-Purpose:
+Purpose: Develop class skeleton for CPP Parking System. This includes attributes and their types.
+A method skeleton is also included.
 */
 
 import java.io.*;
 import java.util.*;
 
+class Skeleton
+{
+  public class static void main(String[] args)
+  {
+    
+  }//end main
+}
