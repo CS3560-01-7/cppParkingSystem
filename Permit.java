8374efbf4172c413443db4742dadc4a34ec5fec2
LynnTaka <36210389+LynnTaka@users.noreply.github.com>
2022-03-16 10:54:58 -0700

    Update Permit.java

--- Permit.java
+++ Permit.java
@@ -1,9 +1,16 @@
+/*
+Class name: Permit.java
+Contributers: Lynn Takahashi, Ethan Vazquez, Ly Rivera, Amal Anu, Sergey Hambardzumyan
+Purpose: 
+*/
+
+import javax.swing.*;
 import java.io.*;
 import java.util.*;
 
 class Permit
 {
-  int permitNumber;
+  int permitNumber;//pk
   String permitType;  
   
 }
