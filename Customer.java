c79e003c4fa4d58cfd312bbac2e966a451124d40
LynnTaka <36210389+LynnTaka@users.noreply.github.com>
2022-03-16 10:50:14 -0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,12 +1,23 @@
+/*
+Class name: Customer.java
+Contributers: Lynn Takahashi, Ethan Vazquez, Ly Rivera, Amal Anu, Sergey Hambardzumyan
+Purpose: Customer class, recieves and compares user information to system database's info.
+The customer class holds the attributes: broncoID (primary key), first name, last name, email
+address, and 
+*/
+
 import java.io.*;
 import java.util.*;
 
 class Customer
 {
-  int bronkoID;
+  //attributes
+  int broncoID;//pk
   String firstName;
-  String lstName;
+  String lastName;
   String emailAddress;
   
+  //constructor
   
+  //methods 
 }
