2cc821a555f9273b5e1d07e9866d01dc3598fed8
Papavazquez <95202332+Papavazquez@users.noreply.github.com>
2022-03-16 15:03:54 -0700

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -9,11 +9,105 @@ import java.util.*;
 
 class Vehicle
 {
-  String licensePlate;//pk
-  String vehicleType;
-  String vehicleMake;
-  String vehicleModel;
-  String vehicleColor;
+  //attributes
+  private String licensePlate;//pk
+  private String vehicleType;
+  private String vehicleMake;
+  private String vehicleModel;
+  private String vehicleColor;
+  
+  
+  //constructor
+  public Vehicle()
+  {
+  }
+  
+  //@param lp The value to store in licensePlate
+  //setLicensePlate() method stores a string in the licensePlate field
+    
+  public void setLicensePlate(String lp)
+  {
+    licensePlate = lp;
+  }
+  
+  //@param vt The value to store in vehicleType
+  //setVehicleType() method stores a string in the vehicleType field
+  
+  public void setVehicleType(String vt)
+  {
+    vehicleType = vt;
+  }
+  
+  //@param vmak Value to store in vehicleMake
+  //setVehicleMake() method stores a string in the vehicleMake field
+  
+  public void setVehicleMake(String vmak)
+  {
+    vehicleMake = vmak;
+  }
+  
+  //@param vmod Value to store in vehicleModel
+  //setVehicleModel() method stores a string in the vehicleModel field
+  
+  public void setVehicleModel(String vmod)
+  {
+    vehicleModel = vmod;
+  }
+  
+  //@param vc Value to store in vehicleColor
+  //setVehicleColor() method stores a string in the vehicleColor field
+  
+  public void setVehicleColor(String vc)
+  {
+    vehicleColor = vc;
+  }
+  
+  //accessors
+  /**
+  getLicensePlate() method returns licensePlate
+  @return Value of licensePlate field
+  */
+  public String getLicensePlate()
+  {
+    return licensePlate:
+  }
+  
+  /**
+  getVehicleType() method returns vehicleType
+  @return Value of vehicleType field
+  */
+  public String getVehicleType()
+  {
+    return vehicleType;
+  }
+  
+   /**
+  getVehicleMake() method returns vehicleMake
+  @return Value of vehicleMake field
+  */
+  public String getVehicleMake()
+  {
+    return vehicleMake;
+  }
+  
+   /**
+  getVehicleModel() method returns vehicleModel
+  @return Value of vehicleModel field
+  */
+  public String getVehicleModel()
+  {
+    return vehicleModel;
+  }
+  
+   /**
+  getVehicleColor() method returns vehicleColor
+  @return Value of vehicleColor field
+  */
+  public String getVehicleColor()
+  {
+    return vehicleColor;
+  }
+  
   
   
 }
