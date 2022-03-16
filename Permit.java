863a7f4e5c2b546bec1ecc0af15f6b6ed9447d28
Papavazquez <95202332+Papavazquez@users.noreply.github.com>
2022-03-16 15:45:25 -0700

    Update Permit.java

--- Permit.java
+++ Permit.java
@@ -10,7 +10,51 @@ import java.util.*;
 
 class Permit
 {
-  int permitNumber;//pk
+  int permitNumber;//pn
   String permitType;  
+  
+  //constructor
+  public Permit()
+  {
+  }
+  
+  //methods
+  //mutators
+  /**
+  setPermitNumber() method stores a value in the permitNumber field
+  @param pn The value to store in permitNumber
+  */
+  public void setPermitNumber(int pn)
+  {
+    permitNumber = pn;
+  }
+  
+  /**
+  setpermitType() method stores a value in the permitType field
+  @param pt The value to store in permitType
+  */
+  public void setPermitType(String pt)
+  {
+    permitType = pt;
+  }
+  
+  //accessors
+  /**
+  getPermitNumber() method returns a object's permitNumber
+  @return The value in the permitNumber field
+  */
+  public int getPermitNumber()
+  {
+    return permitNumber;
+  }
+  
+  /**
+  getPermitType() method returns a object's permitType
+  @return The value in the permitType field
+  */
+  public String getPermitType()
+  {
+    return permitType;
+  }
   
 }
