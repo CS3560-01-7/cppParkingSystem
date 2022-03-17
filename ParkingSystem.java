c664617056d35c8ef229a3e8a7f9b98d5aacf020
lyhoangrivera <80252641+lyhoangrivera@users.noreply.github.com>
2022-03-17 11:48:32 -0700

    Update ParkingSystem.java

--- ParkingSystem.java
+++ ParkingSystem.java
@@ -3,8 +3,11 @@ Project name: ParkingSystem.java
 
 Contributers: Lynn Takahashi, Ethan Vazquez, Ly Rivera, Amal Anu, Sergey Hambardzumyan
 
-Purpose: main method for the cpp parking systems project.  this system will allow users to purchase, look up, and update 
-their parking permit for a specific semester.  
+Purpose: main method for the cpp parking systems project.
+This system will allow users to purchase, look up, and update their parking permit for a specific semester.  
+In the main class, there are lines of code that allow custumer to enter their BroncoID to login.
+It will also have a drop-down menu of permit Type (long/shor semester). Showing custumer permit's price.
+And finally, let custumer enter their credit card's information to pay. 
 */
 
 import java.io.*;
