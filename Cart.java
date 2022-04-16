d8be67b2220efbc9f75d83d97344953c5e4207a5
LynnTaka <36210389+LynnTaka@users.noreply.github.com>
2022-04-15 18:59:01 -0700

    Update Cart.java

--- Cart.java
+++ Cart.java
@@ -1,6 +1,31 @@
+/*
+Class name: Cart.java
+Contributers: Lynn Takahashi
+Purpose: Cart class acts an association class between PaymentMethod,
+Permit, and Customer.
+
+*/
+
 import java.util.*;
 
-class Cart
+public class Cart
 {
+  private ArrayList<Permit> cart = new ArrayList<Permit>();
+  private String customerName;
   
+  public Cart()
+  {
+    
+  }
+  
+  public void addPermit(Permit p)
+  {
+    cart.add(p);
+  }
+  
+  //remove permit???
+  public void removePermit()
+  {
+    cart.remove(0);
+  }
 }//end class
