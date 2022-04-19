fb825ad1151a396d7b5c05ed3af1e68974fd9010
LynnTaka <36210389+LynnTaka@users.noreply.github.com>
2022-04-18 17:02:54 -0700

    Update ParkingSystem.java

--- ParkingSystem.java
+++ ParkingSystem.java
@@ -8,10 +8,13 @@ This system will allow users to purchase, look up, and update their parking perm
 In the main class, there are lines of code that allow customer to enter their BroncoID to login.
 It will also have a drop-down menu of permit Type (long/short semester). Showing customer permit's price.
 And finally, let customer enter their credit card's information to pay. 
+
+put all user prompts here...
 */
 
 import java.io.*;
 import java.util.*;
+import java.swing.*;
 
 class ParkingSystem
 {
