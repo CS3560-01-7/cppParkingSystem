38ce9f8b56aa4177530e7f51a45457e6703ec3f7
LynnTaka <36210389+LynnTaka@users.noreply.github.com>
2022-04-21 00:03:03 -0700

    Create PaymentMethodDB.java

---
+++ PaymentMethodDB.java
@@ -0,0 +1,17 @@
+/*
+Class name: PaymentMethodDB.java
+Contributers: Lynn Takahashi, Ethan Vazquez, Ly Rivera, Amal Anu, Sergey Hambardzumyan
+Purpose: PaymentMethodDB used as a database accessor.
+*/
+
+import java.io.*;
+import java.util.*;
+
+class PaymentMethodDB
+{
+  //select payment method saved
+  
+  //save new payment method
+  
+  //
+}
