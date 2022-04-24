55c0c0cbfef9758ea2c3145373628bc535a05ee5
LynnTaka <36210389+LynnTaka@users.noreply.github.com>
2022-04-23 20:03:00 -0700

    Create CustomerDB.java

---
+++ CustomerDB.java
@@ -0,0 +1,52 @@
+package cpp_parking_system;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class CustomerDB {
+
+public static Customer selectCustomer(Customer customer) throws Exception
+{
+	try {
+		Connection con = getConnection();
+		
+		PreparedStatement statement = con.prepareStatement("SELECT firstName, lastName, emailAddress, address, checkStudent FROM customer WHERE broncoID =" + customer.getBroncoID());
+		
+		ResultSet result = statement.executeQuery();
+		
+		while(result.next())
+		{
+			customer.setFirstName(result.getString("firstName"));
+			customer.setLastName(result.getString("lastName"));
+			customer.setEmailAddress(result.getString("emailAddress"));
+			customer.setAddress(result.getString("address"));
+			customer.setStudent(result.getBoolean("checkStudent"));
+		}
+		
+		return customer;
+	}catch(Exception e) {System.out.println(e);}
+	return null;
+}
+
+public static Connection getConnection() throws Exception
+{
+	try {
+		String driver = "com.mysql.cj.jdbc.Driver";
+		String url = "jdbc:mysql://127.0.0.1:3306/cpp_parking_system";
+		String username = "";
+		String password = "";
+		Class.forName(driver);
+		
+		Connection conn = DriverManager.getConnection(url, username, password);
+		System.out.println("Connected");
+		
+		return conn;
+	}catch (Exception e) {System.out.println(e);}
+	
+	return null;
+}
+
+}
+
