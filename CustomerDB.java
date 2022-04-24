9156da81a962e287b5e2d47241252821ff1993f0
LynnTaka <36210389+LynnTaka@users.noreply.github.com>
2022-04-23 20:15:56 -0700

    Update CustomerDB.java

--- CustomerDB.java
+++ CustomerDB.java
@@ -34,9 +34,9 @@ public static Connection getConnection() throws Exception
 {
 	try {
 		String driver = "com.mysql.cj.jdbc.Driver";
-		String url = "jdbc:mysql://127.0.0.1:3306/cpp_parking_system";
-		String username = "";
-		String password = "";
+		String url = "jdbc:mysql://127.0.0.1:3306/cpp_parking_system";//replace with your own
+		String username = "";//for sql
+		String password = "";//for sql
 		Class.forName(driver);
 		
 		Connection conn = DriverManager.getConnection(url, username, password);
