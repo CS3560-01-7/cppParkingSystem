1d36c168dc1f1d59a89bc7614bcfc78bf7c7594c
LynnTaka <36210389+LynnTaka@users.noreply.github.com>
2022-04-23 23:01:11 -0700

    Create CustomerRevise.java

---
+++ CustomerRevise.java
@@ -0,0 +1,89 @@
+public class CustomerRevise {
+	protected int broncoID;
+	protected String firstName;
+	protected String lastName;
+	protected String emailAddress;
+	protected String address;
+	protected int vehicleCount;
+	protected boolean student;
+	protected Vehicle vehicle;
+	protected PaymentMethod payment;
+	private CustomerDB db = new CustomerDB();
+
+	//constructor
+	Customer()
+	{
+		broncoID=0;
+		firstName = "";
+		lastName = "";
+		emailAddress = "";
+		address = "";
+		vehicleCount = 0;
+		vehicle = new Vehicle();
+		payment = new PaymentMethod();
+	}
+		
+	//getters
+	public int getBroncoID()
+	{
+		return broncoID;
+	}
+		
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	public String getLastName()
+	{
+		return lastName;
+	}
+	public String getFullName()
+	{
+		return firstName + " " + lastName;
+	}
+	public String getEmailAddress()
+	{
+		return emailAddress;
+	}
+	public String getAddress()
+	{
+		return address;
+	}
+	public boolean getStudent()
+	{
+		return student;
+	}
+		
+	//setters
+	public void setBroncoID(int id)
+	{
+		broncoID = id;
+	}
+	public void setFirstName(String name)
+	{
+		firstName = name;
+	}
+	public void setLastName(String name)
+	{
+		lastName = name;
+	}
+	public void setEmailAddress(String address)
+	{
+		emailAddress = address;
+	}
+	public void setStudent(boolean s)
+	{
+		student = s;
+	}
+
+	public void setAddress(String ad) {
+		address = ad;
+		
+	}
+		
+	//connection
+	public void getInfoFromDB() throws Exception
+	{
+		db.selectCustomer(this);
+	}
+}
