b481392be1c22b04e793330a4470d2b4dc1e1bb4
LynnTaka <36210389+LynnTaka@users.noreply.github.com>
2022-04-25 14:00:27 -0700

    Update PaymentMethodDB.java

--- PaymentMethodDB.java
+++ PaymentMethodDB.java
@@ -9,9 +9,21 @@ import java.util.*;
 
 class PaymentMethodDB
 {
-  //select payment method saved
-  
-  //save new payment method
-  
-  //
+public static Connection getConnection() throws Exception
+{
+	try {
+		String driver = "com.mysql.cj.jdbc.Driver";
+		String url = "jdbc:mysql://127.0.0.1:3306/cpp_parking_system";//replace with your own
+		String username = "";//for sql
+		String password = "";//for sql
+		Class.forName(driver);
+		
+		Connection conn = DriverManager.getConnection(url, username, password);
+		System.out.println("Connected");
+		
+		return conn;
+	}catch (Exception e) {System.out.println(e);}
+	
+	return null;
+}
 }
