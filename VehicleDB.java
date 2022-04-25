8ef1441e5aa3a689a70f8565bb0fc0a18a7bf6e6
LynnTaka <36210389+LynnTaka@users.noreply.github.com>
2022-04-25 13:59:40 -0700

    Update VehicleDB.java

--- VehicleDB.java
+++ VehicleDB.java
@@ -1,4 +1,21 @@
-class VehicleDB
+public class VehicleDB {
+
+public static Connection getConnection() throws Exception
 {
-  
+	try {
+		String driver = "com.mysql.cj.jdbc.Driver";
+		String url = "jdbc:mysql://127.0.0.1:3306/cpp_parking_system";//replace with your own
+		String username = "";//for sql
+		String password = "";//for sql
+		Class.forName(driver);
+		
+		Connection conn = DriverManager.getConnection(url, username, password);
+		System.out.println("Connected");
+		
+		return conn;
+	}catch (Exception e) {System.out.println(e);}
+	
+	return null;
+}
+
 }
