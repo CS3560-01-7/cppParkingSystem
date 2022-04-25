9b98e48d2c4d282b7e1272a06a343d700628d9e0
LynnTaka <36210389+LynnTaka@users.noreply.github.com>
2022-04-25 16:35:01 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,21 +12,10 @@ And finally, let customer enter their credit card's information to pay.
 put all user prompts here...
 */
 package application;
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 
 public class Main extends Application{
-	Scene scene1, scene2;
 	static Customer customer = new Customer();
 	
 	public static void main(String[] args) {
@@ -34,52 +23,9 @@ public class Main extends Application{
 	}
 	@Override
 	public void start(Stage primaryStage) {
-		//set up the stage and button
-		Button button = new Button();
-		button.setText("Next");
-		primaryStage.setTitle("CPP LOG IN");
-		Label label = new Label ("Enter your broncoID and password");
+		//set title 
+		NewWindow window = new NewWindow();
+		window.BroncoID(primaryStage, customer);
 		
-		//Form
-		TextField id = new TextField();
-		
-		
-		button.setOnAction(new EventHandler<ActionEvent>()//whenever button is clicked code to handle is in this class
-		{
-			@Override
-			public void handle (ActionEvent event)
-			{
-				customer.setBroncoID(Integer.parseInt(id.getText()));
-				try {
-					customer.getInfoFromDB();
-					System.out.println(customer.getFullName());
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-			}
-		});//go here when pressed
-		
-		
-		//layout 1 stack objects on top of one another
-		VBox layout = new VBox();
-		layout.getChildren().addAll(label, id, button);
-		
-		//display
-		scene1 = new Scene(layout, 600, 500);
-		primaryStage.setScene(scene1);
-		primaryStage.show();
-		
-		/*new EventHandler<ActionEvent>()//whenever button is clicked code to handle is in this class
-		{
-			@Override
-			public void handle (ActionEvent event)
-			{
-				
-			}
-		});
-		*/
 	}
 }
-	
