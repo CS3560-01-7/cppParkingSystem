55b4af08753000544ccb520ed9acc8a40a6e0007
LynnTaka <36210389+LynnTaka@users.noreply.github.com>
2022-04-26 16:23:56 -0700

    Update PaymentMethod.java

--- PaymentMethod.java
+++ PaymentMethod.java
@@ -13,136 +13,157 @@ basics completed.
 
 import java.io.*;
 import java.util.*;
+package application;
 
-class PaymentMethod
-{
-  //attributes
-  private int cardNumber;//pk
-  private int securityCode;
-  private int expDate;
-  private String name;
-  private String billingAddress; 
-  private PaymentMethodDB db;
-  
-  //constructor
-  public PaymentMethod()
-  {
-    cardNumber = 0;
-    securityCode = 0;
-    expDate = 0;
-    name = "";
-    billingAddress = "";
-  }
-  public PaymentMethod(int cardNumber, int securityCode, int expDate, String name, String billingAddress)
-  {
-    this.cardNumber = cardNumber;
-    this.securityCode = securityCode;
-    this.expDate = expDate;
-    this.name = name;
-    this.billingAddress = billingAddress;
-  }
-  //methods 
-  //mutators
-  /**
-  setCreditCard method stores a value into creditCard field
-  @param cc The value to store in creditCard
-  */
-  public void setCardNumber(int cc)
-  {
-    cardNumber = cc;
-  }
-  
-  
-  /**
-  setSecurityCode method stores a value into securityCode field
-  @param sc The value to store in securityCode
-  */
-  public void setSecurityCode(int sc)
-  {
-    securityCode = sc;
-  }
-  
-  
-  /**
-  setExpDate method stores a value into expDate field
-  @param ba The value to store in expDate
-  */
-  public void setExpDate(int ed)
-  {
-    expDate = ed;
-  }
-  
-  
-  /**
-  setName method stores a value into name field
-  @param ba The value to store in name
-  */
-  public void setName(String n)
-  {
-    name = n;
-  }
-  /**
-  setBillingAddress method stores a value into billingAddress field
-  @param ba The value to store in billingAddress
-  */
-  public void setBillingAddress(String ba)
-  {
-    billingAddress = ba;
-  }
-  
-  //accessors
-  /**
-  getCardNumber method returns PaymentMethod object's cardNumber
-  @return The value in cardNumber
-  */
-  public int getCardNumber()
-  {
-    return cardNumber;
-  }
-  
-  /**
-  getSecurityCode method returns PaymentMethod object's securityCode
-  @return The value in securityCode
-  */
-  public int getSecurityCode()
-  {
-    return securityCode;
-  }
-  
-  
-  /**
-  getExpDate method returns PaymentMethod object's expDate
-  @return The value in expDate
-  */
-  public int getExpDate()
-  {
-    return expDate;
-  }
-  
-  /**
-  getName method returns PaymentMethod object's name
-  @return The value in name
-  */
-  public String getName()
-  {
-    return name;
-  }
-  
-  /**
-  getBillingAddress method returns PaymentMethod object's billingAddress
-  @return The value in billingAddress
-  */
-  public String getBillingAddress()
-  {
-    return billingAddress;
-  }
- 
-  //connection
-	/**
-	public void getInfoFromDB() throws Exception
-	{
-		db.selectPaymentMethod(this);//keep in mind this may return nothing so we need an add method if it returns nothing
-	}
-  */
-  
+public class PaymentMethod {
+	private long cardNumber;//pk
+	  private int securityCode;
+	  private int expDate;
+	  private String firstName;
+	  private String lastName;
+	  private String billingAddress; 
+	  private int zip;
+	  
+	  //private PaymentMethodDB db;
+	  
+	  //constructor
+	  public PaymentMethod()
+	  {
+	    cardNumber = 0;
+	    securityCode = 0;
+	    expDate = 0;
+	    firstName = "";
+	    lastName = "";
+	    billingAddress = "";
+	  }
+	  public PaymentMethod(long cardNumber, int securityCode, int expDate, String firstName, String lastName, String billingAddress)
+	  {
+	    this.cardNumber = cardNumber;
+	    this.securityCode = securityCode;
+	    this.expDate = expDate;
+	    this.firstName = firstName;
+	    this.lastName = lastName;
+	    this.billingAddress = billingAddress;
+	  }
+	  //methods 
+	  //mutators
+	  /**
+	  setCreditCard method stores a value into creditCard field
+	  @param cn The value to store in creditCard
+	  */
+	  public void setCardNumber(long cn)
+	  {
+	    cardNumber = cn;
+	  }
+	  
+	  
+	  /**
+	  setSecurityCode method stores a value into securityCode field
+	  @param sc The value to store in securityCode
+	  */
+	  public void setSecurityCode(int sc)
+	  {
+	    securityCode = sc;
+	  }
+	  
+	  
+	  /**
+	  setExpDate method stores a value into expDate field
+	  @param ba The value to store in expDate
+	  */
+	  public void setExpDate(int ed)
+	  {
+	    expDate = ed;
+	  }
+	  
+	  
+	  /**
+	  setFirstName method stores a value into name field
+	  @param ba The value to store in name
+	  */
+	  public void setFirstName(String n)
+	  {
+	    firstName = n;
+	  }
+	  /**
+	  setLastName method stores a value into name field
+	  @param ba The value to store in name
+	  */
+	  public void setLastName(String n)
+	  {
+	    lastName = n;
+	  }
+	  /**
+	  setBillingAddress method stores a value into billingAddress field
+	  @param ba The value to store in billingAddress
+	  */
+	  public void setBillingAddress(String ba)
+	  {
+	    billingAddress = ba;
+	  }
+	  
+	  //accessors
+	  /**
+	  getCardNumber method returns PaymentMethod object's cardNumber
+	  @return The value in cardNumber
+	  */
+	  public long getCardNumber()
+	  {
+	    return cardNumber;
+	  }
+	  
+	  /**
+	  getSecurityCode method returns PaymentMethod object's securityCode
+	  @return The value in securityCode
+	  */
+	  public int getSecurityCode()
+	  {
+	    return securityCode;
+	  }
+	  
+	  
+	  /**
+	  getExpDate method returns PaymentMethod object's expDate
+	  @return The value in expDate
+	  */
+	  public int getExpDate()
+	  {
+	    return expDate;
+	  }
+	  
+	  /**
+	  getName method returns PaymentMethod object's name
+	  @return The full name
+	  */
+	  public String getName()
+	  {
+	    return firstName + " " + lastName;
+	  }
+	  
+	  /**
+	  getFirstName method returns PaymentMethod object's name
+	  @return The value in firstName
+	  */
+	  public String getFirstName()
+	  {
+	    return firstName;
+	  }
+	  
+	  /**
+	  getFirstName method returns PaymentMethod object's name
+	  @return The value in firstName
+	  */
+	  public String getLastName()
+	  {
+	    return lastName;
+	  }
+	  /**
+	  getBillingAddress method returns PaymentMethod object's billingAddress
+	  @return The value in billingAddress
+	  */
+	  public String getBillingAddress()
+	  {
+	    return billingAddress;
+	  }
+	 
 }
