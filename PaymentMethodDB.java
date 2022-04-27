309de299c18f78c7cfa3673bee8a33c73fafee15
sergeyham <101605028+sergeyham@users.noreply.github.com>
2022-04-27 16:44:36 -0700

    Update PaymentMethodDB.java

--- PaymentMethodDB.java
+++ PaymentMethodDB.java
@@ -5,25 +5,54 @@ Purpose: PaymentMethodDB used as a database accessor.
 */
 
 import java.io.*;
+import java.sql.*;
 import java.util.*;
 
 class PaymentMethodDB
 {
-public static Connection getConnection() throws Exception
-{
-	try {
-		String driver = "com.mysql.cj.jdbc.Driver";
-		String url = "jdbc:mysql://127.0.0.1:3306/cpp_parking_system";//replace with your own
-		String username = "";//for sql
-		String password = "";//for sql
-		Class.forName(driver);
-		
-		Connection conn = DriverManager.getConnection(url, username, password);
-		System.out.println("Connected");
-		
-		return conn;
-	}catch (Exception e) {System.out.println(e);}
-	
-	return null;
-}
+    public static PaymentMethod selectPaymentMethod(Customer customer) throws Exception
+    {
+        PaymentMethod paymentMethod = new PaymentMethod();
+        try {
+            Connection conn = getConnection();
+
+            Statement st = conn.createStatement();
+            String query ="select * from PaymentMethod WHERE broncoID = " + customer.getBroncoID();
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                paymentMethod.setCardNumber(rs.getLong("cardNumber"));
+                paymentMethod.setSecurityCode(rs.getInt("securityCode"));
+                paymentMethod.setExpDateM(rs.getInt("expirationDateMonth"));
+                paymentMethod.setExpDateY(rs.getInt("expirationDateYear"));
+                paymentMethod.setFirstName(rs.getString("firstName"));
+                paymentMethod.setLastName(rs.getString("lastName"));
+                paymentMethod.setBillingAddress(rs.getString("address"));
+
+
+                return paymentMethod;
+            }
+            st.close();
+            conn.close();
+
+        }catch(Exception e) {System.out.println(e);}
+        return null;
+    }
+
+    public static Connection getConnection() {
+        try {
+            String driver = "com.mysql.cj.jdbc.Driver";
+            String url = "jdbc:mysql://localhost:3306/cpp_parking_system";
+            String username = "root";
+            String password = "****";//password
+            Class.forName(driver);
+
+            Connection conn = DriverManager.getConnection(url, username, password);
+            return conn;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return null;
+
+    }
 }
