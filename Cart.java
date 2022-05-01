bfc38486bfe84c1db59f218262e95621a2691a4e
LynnTaka <36210389+LynnTaka@users.noreply.github.com>
2022-05-01 10:42:09 -0700

    Update Cart.java

--- Cart.java
+++ Cart.java
@@ -11,53 +11,41 @@ import java.util.*;
 public class Cart
 {
   private Permit permit;
-  private Customer customer;
-  private PaymentMethod payment;
-  
-  public Cart()
-  {
-    permit = new Permit();
-    customer = new Customer();
-    payment = new PaymentMethod();
-  }
-  
-  public Cart(Permit p, Customer c, PaymentMethod pm)
-  {
-    permit = p;
-    customer = c;
-    payment = pm;
-  }
-  
-  
-  //getters and setters
-  public void setPermit(Permit p)
-  {
-    permit = p;
-  }
-  
-  public void setCustomer(Customer c)
-  {
-    customer = c;
-  }
-  
-  public void setPaymentMethod(PaymentMethod pm)
-  {
-    payment = pm;
-  }
-  
-  public Permit getPermit()
-  {
-    return permit;
-  }
-  
-  public Customer getCustomer()
-  {
-    return customer;
-  }
-  
-  public PaymentMethod getPaymentMethod()
-  {
-    return payment;
-  }
+	  private Customer customer;
+	  
+	  public Cart()
+	  {
+	    permit = new Permit();
+	    customer = new Customer();
+	  }
+	  
+	  public Cart(Permit p, Customer c)
+	  {
+	    permit = p;
+	    customer = c;
+	  }
+	  
+	  
+	  //getters and setters
+	  public void setPermit(Permit p)
+	  {
+	    permit = p;
+	  }
+	  
+	  public void setCustomer(Customer c)
+	  {
+	    customer = c;
+	  }
+	  
+	  public Permit getPermit()
+	  {
+	    return permit;
+	  }
+	  
+	  public Customer getCustomer()
+	  {
+	    return customer;
+	  }
+	  
   
 }//end class
