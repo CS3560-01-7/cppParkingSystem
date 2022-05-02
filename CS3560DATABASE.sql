727384bc63d0edd89be3bdc3ddaf5677657cbe2e
LynnTaka <36210389+LynnTaka@users.noreply.github.com>
2022-05-02 16:44:34 -0700

    Update CS3560DATABASE.sql

--- CS3560DATABASE.sql
+++ CS3560DATABASE.sql
@@ -1,26 +1,22 @@
-CREATE DATABASE CPP_PARKING_SYSTEM;
-
 USE CPP_PARKING_SYSTEM;
+DROP TABLE PaymentMethod;
+DROP TABLE Permit;
+DROP TABLE Rate;
+DROP TABLE Vehicle;
+DROP TABLE Customer;
 
 CREATE TABLE Customer (
 	broncoID INT (9),
     firstName VARCHAR (20),
-    lastName VARCHAR (20), 
+    lastName VARCHAR (50),
     emailAddress VARCHAR (50),
-    address VARCHAR(50),
+    address VARCHAR (50),
     checkStudent BOOLEAN,
-    PRIMARY KEY (broncoID));
-    
-INSERT INTO Customer VALUES 
-('111222333', 'Scott', 'Butter', 'scott_butter@cpp.edu', '3402 Central Park', 1),
-('222333444', 'Celine', 'Swift', 'celine_swift@cpp.edu', '1108 Beverly Hills', 1),
-('333444555', 'Olivia', 'Rodrigo', 'olivia_rodrigo@cpp.edu', '999 Catarina Island', 1);
-
-SELECT * FROM Customer;
-DROP TABLE Customer;
+    PRIMARY KEY (broncoID)
+);
 
 CREATE TABLE PaymentMethod (
-    broncoID INT (9),
+    customerbroncoID INT (9),
     cardNumber BIGINT (19),
     securityCode INT (3),
     expirationDateMonth INT (2),
@@ -28,76 +24,50 @@ CREATE TABLE PaymentMethod (
     firstName VARCHAR (20),
     lastName VARCHAR (50),
     address VARCHAR (50),
-    FOREIGN KEY (broncoID) REFERENCES Customer (broncoID),
-    PRIMARY KEY (cardNumber, broncoID));
-
-INSERT INTO PaymentMethod VALUES
-('111222333', '345298765', '345', '09', '2025', 'Scott', 'Butter', '3402 Central Park'),
-('222333444', '897342658', '122', '12', '2026', 'Celine', 'Swift', '1108 Beverly Hills'),
-('333444555', '834982344', '089', '01', '2028', 'Olivia', 'Rodrigo', '999 Catarina Islands');
-
-SELECT * FROM PaymentMethod;
-DROP TABLE PaymentMethod;
-
+    FOREIGN KEY (customerbroncoID) REFERENCES Customer (broncoID),
+    PRIMARY KEY (cardNumber, customerbroncoID)
+);
 CREATE TABLE Vehicle (
-    vin VARCHAR (17),
-	broncoID INT (9),
-    licensePlate VARCHAR (20),
+	vin VARCHAR (17),
+    broncoID INT (9),
+    licensePlate VARCHAR (7),
     vehicleType VARCHAR (20),
     vehicleMake VARCHAR (20),
     vehicleColor VARCHAR (15),
     vehicleYear INT (4),
     state VARCHAR (15),
     FOREIGN KEY (broncoID) REFERENCES Customer (broncoID),
-    PRIMARY KEY (vin));
-
-INSERT INTO Vehicle VALUES
-('1HGBH41JXMN109186', '111222333', 'ANY TEXT', 'Sedan', 'Jeep', 'White', '2020', 'California'),
-('1AUIE64JCLS358308', '222333444', 'SUNKISS', 'SUV', 'HyunDai', 'Black', '2019', 'Nevada'),
-('4DKSG08JFCW275927', '333444555', 'YES MOM', 'Convertible', 'Toyoda', 'Red', '2018', 'Washington');
-
-SELECT * FROM Vehicle;
-DROP TABLE vehicle;
+    PRIMARY KEY (vin)
+);
 
 CREATE TABLE Rate (
-	rateID INT (1),
-    price INT (3),
-    PRIMARY KEY (rateID));
-
-INSERT INTO Rate VALUES
-('1', '100'),
-('2', '200'),
-('3', '300');
-
-SELECT * FROM Rate;
-DROP TABLE rate;
+	rateID INT (2),
+    rate INT (3),
+    rateDescription VARCHAR (50),
+    studentcheck BOOLEAN,
+    PRIMARY KEY (rateID)
+);
 
 CREATE TABLE Permit (
-	permitNumber INT (9),
+	permitID INT (9),
     rateID INT (2),
+    vin VARCHAR (17),
+    FOREIGN KEY (vin) REFERENCES Vehicle (vin),
     FOREIGN KEY (rateID) REFERENCES Rate (rateID),
-    PRIMARY KEY (permitNumber));
-    
-INSERT INTO Permit VALUES
-('111111111', '1'),
-('222222222', '2'),
-('333333333', '3');
-
-SELECT * FROM Permit;
-DROP TABLE permit;
+    PRIMARY KEY (permitID)
+);
 
 
-CREATE TABLE Cart (
-	orderNumber BIGINT (12),
-    broncoID INT (9),
-    FOREIGN KEY (broncoID) REFERENCES Customer (broncoID),
-    PRIMARY KEY (orderNumber));
 
-INSERT INTO Cart VALUES 
-('343787612345', '111222333'),
-('356257864246', '222333444'),
-('345672345098', '333444555');
+INSERT INTO Customer VALUES ('111111111', 'Lynn', 'Takahashi','pizzahut@gmail.com','123 Sesame Street',TRUE),
+('111111112','Professor','Poopypants','professorpoopy@gmail.com','246 Captain Underpants', FALSE),
+('111111113','Ethan','Vazquez','evaq@gmail.com','541 Vazqueef Drive', FALSE);
+INSERT INTO PaymentMethod VALUES ('111111111','123456789','123','11','25','Lynn','Takahashi','123 Sesame Street');
+INSERT INTO Vehicle VALUES ('12345678901234567','111111111','ABCDEFG','Corolla','Toyota','gray','2012', 'CA');
+INSERT INTO Rate VALUES ('1', '150', 'STUDENT FALL MO/WE/FRI', true),
+('2', '150', 'STUDENT FALL TU/TH', true),('3', '200', 'STUDENT FALL WEEK', true),
+('4', '100', 'FACULTY FALL WEEK', false);
+INSERT INTO Permit values ('1', '1', '12345678901234567');
 
-SELECT * FROM Cart;    
-DROP TABLE Cart;
-    
+
+SELECT * FROM Rate;
