3dddf62472885db9d09789f439c8474621a0604c
sergeyham <101605028+sergeyham@users.noreply.github.com>
2022-05-04 16:19:32 -0700

    Update Rate.java

--- Rate.java
+++ Rate.java
@@ -9,92 +9,92 @@ Rate calls Permit objects
 package application;
 
 public class Rate {
-	//attributes
-	private int rateID;
-	private int rate;
-	private String description;
-	private boolean studentcheck;
-	RateDB db = new RateDB();
-  
-	//constructor
-	public Rate()
-	{
-		
-	}
-  
-	//methods
-	//mutators
-	/**
-	setRate method stores a value in the rate field
-	@param r The value to store in rate
-	 */
-	public void setRateID(int r)
-	{
-		this.rateID = r;
-	}
-	
-	/**
-	setRate method stores a value in the rate field
-	@param r The value to store in rate
-	 */
-	public void setRate(int r)
-	{
-		this.rate = r;
-	}
-  
-	/**
-	setDescription method stores description of rate
-	@param d The value to store in longSemester
-	 */
-	public void setDescription(String d)
-	{
-		this.description = d;
-	}
-	
-	/**
- 	Faculty method checks status of the user and decides what rate to offer
- 	@return Boolean value
-	 */
-	public void setStudent(boolean s) 
-	{
-		this.studentcheck = s;
-	}
-  
-	/**
-	getRate method returns rate
-	@return The value in the rate field
-	 */
-	public int getRateID()
-	{
-		return this.rateID;
-	}
-	
-	/**
-	getRate method returns rate
-	@return The value in the rate field
-	 */
-	public int getRate()
-	{
-		return this.rate;
-	}
-  
-	/**
-	getFacultyRate method returns a facultyRate
-	@return The value in the facultyRate field
-	 */
-	public String getDescription()
-	{
-        return this.description;
+	 //attributes
+    private int rateID;
+    private int rate;
+    private String rateDescription;
+    private boolean studentcheck;
+    RateDB ratedb = new RateDB();
+
+    //constructor
+    public Rate()
+    {
+
     }
-  
-	/**
- 	Faculty method checks status of the user and decides what rate to offer
- 	@return Boolean value
-	 */
-	public boolean getStudent() 
-	{
-		return this.studentcheck;
-	}
-	//check if user entered Student or Faculty as their campus status. 
-	//If faculty, return true. If student, return false.
+
+    //methods
+    //mutators
+    /**
+     setRate method stores a value in the rate field
+     @param r The value to store in rate
+     */
+    public void setRateID(int r)
+    {
+        this.rateID = r;
+    }
+
+    /**
+     setRate method stores a value in the rate field
+     @param r The value to store in rate
+     */
+    public void setRate(int r)
+    {
+        this.rate = r;
+    }
+
+    /**
+     setDescription method stores description of rate
+     @param d The value to store in longSemester
+     */
+    public void setRateDescription(String d)
+    {
+        this.rateDescription = d;
+    }
+
+    /**
+     Faculty method checks status of the user and decides what rate to offer
+     @return Boolean value
+     */
+    public void setStudent(boolean s)
+    {
+        this.studentcheck = s;
+    }
+
+    /**
+     getRate method returns rate
+     @return The value in the rate field
+     */
+    public int getRateID()
+    {
+        return this.rateID;
+    }
+
+    /**
+     getRate method returns rate
+     @return The value in the rate field
+     */
+    public int getRate()
+    {
+        return this.rate;
+    }
+
+    /**
+     getFacultyRate method returns a facultyRate
+     @return The value in the facultyRate field
+     */
+    public String getDescription()
+    {
+        return this.rateDescription;
+    }
+
+    /**
+     Faculty method checks status of the user and decides what rate to offer
+     @return Boolean value
+     */
+    public boolean getStudent()
+    {
+        return this.studentcheck;
+    }
+    //check if user entered Student or Faculty as their campus status.
+    //If faculty, return true. If student, return false.
 }
