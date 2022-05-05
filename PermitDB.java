145f6e23ac1ce4c6532103de38fb42482dfc045f
sergeyham <101605028+sergeyham@users.noreply.github.com>
2022-05-04 18:05:24 -0700

    Update PermitDB.java

--- PermitDB.java
+++ PermitDB.java
@@ -1,1 +1,44 @@
 
+// The code should work
+// only insert your connection password
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class PermitDB {
+
+    public static void addPermit(Permit permit)  throws Exception{
+
+        try {
+            Connection conn = getConnection();
+            Statement st = conn.createStatement();
+            String query = "INSERT INTO Permit(rateID,vin)  Values("+permit.getRate().getRateID()+",\'"+permit.getVehicle().getVIN()+"\')";
+            st.executeQuery(query);
+            st.close();
+            conn.close();
+        }catch(Exception e) {
+            System.out.println(e);
+
+        }
+
+    }
+    
+    public static Connection getConnection() {
+        try {
+            String driver = "com.mysql.cj.jdbc.Driver";
+            String url = "jdbc:mysql://localhost:3306/cpp_parking_system";
+            String username = "root";
+            String password = "******";//password
+            Class.forName(driver);
+
+            Connection conn = DriverManager.getConnection(url, username, password);
+            return conn;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return null;
+
+    }
+}
