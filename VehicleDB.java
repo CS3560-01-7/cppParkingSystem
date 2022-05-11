774a64c8aacc4de73903ec1813eed211b93a4a18
LynnTaka <36210389+LynnTaka@users.noreply.github.com>
2022-05-11 12:37:11 -0700

    Update VehicleDB.java

--- VehicleDB.java
+++ VehicleDB.java
@@ -47,7 +47,8 @@ public class VehicleDB {
             		String query = "INSERT INTO Vehicle  Values(\'" +customer.getArrayOfVehicles().get(i).getVIN()+"\',"+customer.getBroncoID()+",\'"
                     +customer.getArrayOfVehicles().get(i).getLicensePlate()+"\',\'"+customer.getArrayOfVehicles().get(i).getVehicleType()+"\',\'"
                     +customer.getArrayOfVehicles().get(i).getVehicleMake()+"\',\'"+customer.getArrayOfVehicles().get(i).getVehicleColor()+"\',"
-                    +customer.getArrayOfVehicles().get(i).getModelYear()+",\'"+customer.getArrayOfVehicles().get(i).getState()+"\')"+customer.getArrayOfVehicles().get(i);
+                    +customer.getArrayOfVehicles().get(i).getModelYear()+",\'"+customer.getArrayOfVehicles().get(i).getState()+"\',"
+                    +customer.getArrayOfVehicles().get(i).getAppears()+")";
             		st.executeUpdate(query);
             	}
             	else {
@@ -94,6 +95,7 @@ public class VehicleDB {
                 vehicle.setVehicleColor(rs.getString("vehicleColor"));
                 vehicle.setModelYear(rs.getInt("vehicleYear"));
                 vehicle.setState(rs.getString("state"));
+                vehicle.setAppears(rs.getBoolean("appears"));
 
                 arrayOfVehicles.add(vehicle);
             }
