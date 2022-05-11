a4fcd679531dd90c587542b6403cf1f0c6f96a13
Papavazquez <95202332+Papavazquez@users.noreply.github.com>
2022-05-11 14:04:48 -0700

    Update PermitDB.java

--- PermitDB.java
+++ PermitDB.java
@@ -3,42 +3,44 @@
 // only insert your connection password
 package application;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.Statement;
 
 public class PermitDB {
 
-public static void addPermit(Permit permit)  throws Exception{
-	Connection conn = getConnection();
+    public static void addPermit(Permit permit)  throws Exception{
+    	
         try {
-            
+            Connection conn = getConnection();
             Statement st = conn.createStatement();
             String query = "INSERT INTO Permit (rateID,vin)  Values("+permit.getRateID()+",\'"+permit.getVIN()+"\')";
             st.executeUpdate(query);
             st.close();
             conn.close();
-        }finally {
-    	    if (conn != null) {
-    	        conn.close();
-    	    }
-    	}
-        
+        }catch(Exception e) {
+            System.out.println(e);
+
+        }
 
     }
     
-    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+    public static Connection getConnection() {
+        try {
             String driver = "com.mysql.cj.jdbc.Driver";
             String url = "jdbc:mysql://localhost:3306/cpp_parking_system";
             String username = "root";
-            String password = "Lynn$anity11";//password
+            String password = "Eman9232";//password
             Class.forName(driver);
 
             Connection conn = DriverManager.getConnection(url, username, password);
-            StackTraceElement[] st = Thread.currentThread().getStackTrace();
-            System.out.println(  "create connection called from " + st[2] );
             return conn;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return null;
 
     }
 }
